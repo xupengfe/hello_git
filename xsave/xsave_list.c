5ca6f286bc47a52b142d16eab7ea2bcf27d24e1e
Pengfei Xu <pengfei.xu@intel.com>
2019-12-05 14:07:51 +0800

    xsave_list.c: add xsave max size print
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- xsave/xsave_list.c
+++ xsave/xsave_list.c
@@ -164,17 +164,23 @@ int cpu_support_xstate_list(void)
 
 int main(void)
 {
-	unsigned int eax, ebx, ecx, edx, cpu_xsave_size;
+	unsigned int eax, ebx, ecx, edx, cpu_xsave_size, cpu_max_xsave_size;
 
 	cpuid_count(XSTATE_CPUID, 1, &eax, &ebx, &ecx, &edx);
 	printf("This CPU could support XSAVE features total size:%d(0x%x)\n",
 		ebx, ebx);
 	cpu_xsave_size=ebx;
 
+	cpuid_count(XSTATE_CPUID, 0, &eax, &ebx, &ecx, &edx);
+	cpu_max_xsave_size=ebx;
+
 	cpu_support_xstate_list();
 	if(cpu_xsave_size != linux_xsave_size)
-		printf("WARN: cpu_xsave_size:%d is not equal to linux_xsave_size:%d\n",
-			cpu_xsave_size, linux_xsave_size);
+		printf("WARN: cpu_xsave_size:%d is not equal to linux_xsave_size:%d, max:%d\n",
+			cpu_xsave_size, linux_xsave_size, cpu_max_xsave_size);
+	else
+		printf("cpu_xsave_size:%d, linux_xsave_size:%d, cpu_max:%d\n",
+			cpu_xsave_size, linux_xsave_size, cpu_max_xsave_size);
 	xstate_dump_leaves();
 	return 0;
 }
