343c0190cf56ef36e5b8144cebe925e78c8d7c42
Pengfei Xu <pengfei.xu@intel.com>
2020-01-20 16:59:55 +0800

    xsave_list.c: improve xsave_list.c code
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- xsave/xsave_list.c
+++ xsave/xsave_list.c
@@ -18,41 +18,48 @@
 #include <linux/types.h>
 #include "xsave_list.h"
 
-static int linux_xsave_size=0;
+static int linux_xsave_size;
+static u64 xfeatures_mask_all;
+
+void usage(char *progname)
+{
+	printf("Usage: %s NUM1 NUM2\n", progname);
+	printf("  NUM1: xstate id\n");
+	printf("  NUM2: xstate id expected size\n");
+}
 
 /*
  * We spell it out in here for xsave xfeature names
  */
-static const char *xfeature_names[] =
-{
-	"x87 floating point registers"	,
-	"SSE registers"			,
-	"AVX registers"			,
-	"MPX bounds registers"		,
-	"MPX CSR"			,
-	"AVX-512 opmask"		,
-	"AVX-512 Hi256"			,
-	"AVX-512 ZMM_Hi256"		,
-	"Processor Trace (unused)"	,
+static const char * const xfeature_names[] = {
+	"x87 floating point registers",
+	"SSE registers",
+	"AVX registers",
+	"MPX bounds registers",
+	"MPX CSR",
+	"AVX-512 opmask",
+	"AVX-512 Hi256",
+	"AVX-512 ZMM_Hi256",
+	"Processor Trace (unused)",
 	"Protection Keys User registers",
 	"PASID state",
-	"Control-flow User registers"	,
-	"Control-flow Kernel registers"	,
-	"Reserved Component (13)"	,
-	"Reserved Component (14)"	,
-	"Reserved Component (15)"	,
-	"Reserved Component (16)"	,
-	"AMX TILE config"		,
-	"AMX TILE data"			,
-	"unknown xstate feature"	,
+	"Control-flow User registers",
+	"Control-flow Kernel registers",
+	"Reserved Component (13)",
+	"Reserved Component (14)",
+	"Reserved Component (15)",
+	"Reserved Component (16)",
+	"AMX TILE config",
+	"AMX TILE data",
+	"unknown xstate feature",
 };
 
 static inline void cpuid_count(u32 id, u32 count,
 		u32 *a, u32 *b, u32 *c, u32 *d)
 {
-	asm volatile(".ifnc %%ebx,%3 ; movl  %%ebx,%3 ; .endif	\n\t"
-		     "cpuid					\n\t"
-		     ".ifnc %%ebx,%3 ; xchgl %%ebx,%3 ; .endif	\n\t"
+	asm volatile(".ifnc %%ebx,%3; movl %%ebx,%3; .endif\n\t"
+		     "cpuid\n\t"
+		     ".ifnc %%ebx,%3; xchgl %%ebx,%3; .endif\n\t"
 		    : "=a" (*a), "=c" (*c), "=d" (*d), "=b" (*b)
 		    : "a" (id), "c" (count)
 	);
@@ -83,7 +90,7 @@ static void xstate_dump_leaves(void)
 	 */
 	for (i = 0; i < XFEATURE_MAX + 10; i++) {
 		cpuid_count(XSTATE_CPUID, i, &eax, &ebx, &ecx, &edx);
-		printf("CPUID[%02x, %02x]: eax=%08x ebx=%08x ecx=%08x edx=%08x\n",
+		printf("CPUID[%02x,%02x]:eax=%08x ebx=%08x ecx=%08x edx=%08x\n",
 			XSTATE_CPUID, i, eax, ebx, ecx, edx);
 	}
 }
@@ -102,40 +109,38 @@ void xfeature_name(u64 xfeature_idx, const char **feature_name)
 static unsigned int get_xfeature_size(int ecx)
 {
 	unsigned int eax, ebx, edx;
-	cpuid_count(XSTATE_CPUID, ecx , &eax, &ebx, &ecx, &edx);
+
+	cpuid_count(XSTATE_CPUID, ecx, &eax, &ebx, &ecx, &edx);
 	return eax;
 }
 
 static void print_xstate_feature(u32 xstate_mask)
 {
 	const char *feature_name;
-	int xfeature_size=0, is_aligned=0;
-
+	int xfeature_size = 0, is_aligned = 0;
 
 	xfeature_name(xstate_mask, &feature_name);
-	is_aligned=xfeature_is_aligned(xstate_mask);
+	is_aligned = xfeature_is_aligned(xstate_mask);
 	if (xstate_mask == 0)
-		xfeature_size=512;
-	else if(xstate_mask == 1)
-		xfeature_size=64;
+		xfeature_size = 512;
+	else if (xstate_mask == 1)
+		xfeature_size = 64;
 	else
-		xfeature_size=get_xfeature_size(xstate_mask);
+		xfeature_size = get_xfeature_size(xstate_mask);
 
-	printf("CPU support XSAVE feature 0x%05x| %04d |   %d   |'%s'\n",
+	printf("CPU support XSAVE 0x%05x| %04d |   %d   |'%s'\n",
 		xstate_mask, xfeature_size, is_aligned, feature_name);
-	linux_xsave_size=linux_xsave_size+xfeature_size;
+	linux_xsave_size = linux_xsave_size+xfeature_size;
 }
 
 int cpu_support_xstate_list(void)
 {
-	unsigned int eax, ebx, ecx, edx, bit_check=0x4000000, i=0;
-
-	u64 xfeatures_mask_all;
+	unsigned int eax, ebx, ecx, edx, bit_check = 0x4000000, i = 0;
 
 	// Verify this CPU could support XSAVE instructions
 	cpuid_count(1, 0, &eax, &ebx, &ecx, &edx);
 	if (!(ecx & bit_check)) {
-		printf("This CPU could not support XSAVE instructions, exit.\n");
+		printf("CPU could not support XSAVE instructions, exit.\n");
 		return 1;
 	}
 	cpuid_count(XSTATE_CPUID, 0, &eax, &ebx, &ecx, &edx);
@@ -149,12 +154,12 @@ int cpu_support_xstate_list(void)
 		xfeatures_mask_all);
 
 	xfeatures_mask_all &= fpu_get_supported_xfeatures_mask();
-	printf("After check FPU suppport, xfeatures_mask_all:%lx\n",
+	printf("After check FPU support, xfeatures_mask_all:%lx\n",
 		xfeatures_mask_all);
-	printf("Align should be CPUID.(EAX=0DH, ECX=xfeature_id):ECX[bit 2].\n");
+	printf("Align for CPUID(EAX=0DH, ECX=xfeature_id):ECX[bit 2].\n");
 
-	printf("CPU support XSAVE feature 0x000ID| Size | Align |'feature_name'\n");
-	for(i=0; i<XFEATURE_MAX; i++) {
+	printf("CPU support XSAVE 0x000ID| Size | Align |'feature_name'\n");
+	for (i = 0; i < XFEATURE_MAX; i++) {
 		if (!(xfeatures_mask_all & (1 << i)))
 			continue;
 		print_xstate_feature(i);
@@ -162,21 +167,72 @@ int cpu_support_xstate_list(void)
 	return 0;
 }
 
-int main(void)
+static int xfeature_enabled(unsigned int xfeature)
+{
+	return !!(xfeatures_mask_all & (1UL << xfeature));
+}
+
+int xstate_size_verify(int xfeature_id, int xstate_exp_size)
+{
+	unsigned int eax, ebx, ecx, edx, xfeature_size = 0;
+
+	cpu_support_xstate_list();
+	if (xfeature_enabled(xfeature_id))
+		printf("CPU could support decimal xstate %d\n", xfeature_id);
+	else {
+		printf("CPU can not support decimal xstate %d\n", xfeature_id);
+		exit(2);
+	}
+	cpuid_count(XSTATE_CPUID, xfeature_id, &eax, &ebx, &ecx, &edx);
+	xfeature_size = eax;
+	if (xfeature_size == xstate_exp_size) {
+		printf("RESULTS:xstate id %d size is as expected:%d\n",
+			xfeature_id, xfeature_size);
+	} else {
+		printf("RESULTS:xstate id %d actual size:%d, expected:%d\n",
+			xfeature_id, xfeature_size, xstate_exp_size);
+		exit(1);
+	}
+}
+
+int main(int argc, char *argv[])
 {
 	unsigned int eax, ebx, ecx, edx, cpu_xsave_size, cpu_max_xsave_size;
+	int xstate_id, xstate_id_size;
+
+	if (argc == 1)
+		usage(argv[0]);
+	else if (argc == 3) {
+		if (sscanf(argv[1], "%d", &xstate_id) != 1) {
+			printf("Invalid xstate_id:%d\n", xstate_id);
+			usage(argv[0]);
+			exit(2);
+		}
+		if (sscanf(argv[2], "%d", &xstate_id_size) != 1) {
+			printf("Invalid xstate_id_size:%d\n", xstate_id_size);
+			usage(argv[0]);
+			exit(2);
+		}
+		printf("Will verify xstate id:%d, size should be:%d\n",
+			xstate_id, xstate_id_size);
+		xstate_size_verify(xstate_id, xstate_id_size);
+		exit(0);
+	} else {
+		usage(argv[0]);
+		exit(2);
+	}
 
 	cpuid_count(XSTATE_CPUID, 1, &eax, &ebx, &ecx, &edx);
-	cpu_xsave_size=ebx;
+	cpu_xsave_size = ebx;
 
 	cpuid_count(XSTATE_CPUID, 0, &eax, &ebx, &ecx, &edx);
-	cpu_max_xsave_size=ebx;
+	cpu_max_xsave_size = ebx;
 
-	printf("This CPU could support XSAVE features total size:%d(0x%x), max:0d%d\n",
+	printf("CPU could support XSAVE total size:%d(0x%x), max:0d%d\n",
 		cpu_xsave_size, cpu_xsave_size, cpu_max_xsave_size);
 	cpu_support_xstate_list();
-	if(cpu_xsave_size != linux_xsave_size)
-		printf("WARN: cpu_xsave_size:%d is not equal to linux_xsave_size:%d, max:%d\n",
+	if (cpu_xsave_size != linux_xsave_size)
+		printf("WARN:cpu_xsave_size:%d not equal to linux:%d, max:%d\n",
 			cpu_xsave_size, linux_xsave_size, cpu_max_xsave_size);
 	xstate_dump_leaves();
 	return 0;
