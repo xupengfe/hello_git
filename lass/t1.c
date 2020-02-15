298d27e827e26f68c1e27ec9f2b8a1647d241f88
Pengfei Xu <pengfei.xu@intel.com>
2020-02-15 16:49:56 +0800

    t1.c and test_vsyscall.c: add some new function
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- lass/t1.c
+++ lass/t1.c
@@ -8,14 +8,19 @@
 int main()
 {
   struct timeval tv, sys_tv;
+  struct timeval *tp = &tv, *stp = &sys_tv;
+  unsigned int hack_a;
 
-  //syscall(SYS_gettimeofday, &sys_tv, NULL);
+
+  printf("SYS_gettimeofday:%d\n", SYS_gettimeofday);
   gettimeofday(&tv, NULL);
-  syscall(SYS_gettimeofday, &sys_tv, NULL);
-  printf("gettimeofday time: %ld sec %ld usec, tv:%p\n",
+  printf("gettimeofday time: %ld sec %ld usec, &tv:%p\n",
     tv.tv_sec, tv.tv_usec, &tv);
-  printf("SYS_gettimeofday: %ld sec %ld usec, sys_tv:%p\n",
-    sys_tv.tv_sec, sys_tv.tv_usec, &sys_tv);
+  syscall(SYS_gettimeofday, &sys_tv, NULL);
+  printf("gettimeofday time: %ld sec %ld usec, &tv:%p\n",
+    tv.tv_sec, tv.tv_usec, &tv);
+  printf("SYS_gettimeofday: %ld sec %ld usec, sys_tv:%p, %p, %p, %p\n",
+    sys_tv.tv_sec, sys_tv.tv_usec, &sys_tv, stp, &stp, &(*stp));
 
   pid_t pid = getpid();
   printf("getpid pid = %d\n", pid);
@@ -23,5 +28,8 @@ int main()
   pid = syscall(SYS_getpid);
   printf("SYS_getpid pid = %d\n", pid);
 
+  hack_a = *(const int *)0xffffffffff600000;
+  printf("0xffffffffff600000:0x%x\n", hack_a);
+  // hack_a = *(const int *)0xffffffffff601000; // will SIGSEGV
   return 0;
 }
