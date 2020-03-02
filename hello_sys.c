a7ef1c5250242b5a7ceb8998558d72b08511db70
Pengfei Xu <pengfei.xu@intel.com>
2020-03-02 16:53:13 +0800

    hello_sys: add include
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- hello_sys.c
+++ hello_sys.c
@@ -1,11 +1,14 @@
 #include <string.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
     char *msg = "Hello, world!\n";
     syscall(SYS_write, 1, msg, strlen(msg));
+
+    printf("SYS_write:%d\n", SYS_write);
 
     return 0;
 }
