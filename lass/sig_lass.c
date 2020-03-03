83eb5354a175242aad89330223d3d9e48d34780f
Pengfei Xu <pengfei.xu@intel.com>
2020-03-03 17:10:30 +0800

    sig_lass.c: improve the code format
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- lass/sig_lass.c
+++ lass/sig_lass.c
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ *  lass_test.c
+ *  Copyright (C) 2020 Intel Corporation
+ *  Author: Pengfei, Xu <pengfei.xu@intel.com>
+ */
 
 #include <stdio.h>
 #include <signal.h>
@@ -8,17 +13,15 @@ void segv_handler(int signum, siginfo_t *si, void *uc)
 {
 	printf("sig:%d, no:%d, err:%d, si_code:%d\n",
 		signum, si->si_signo, si->si_errno, si->si_code);
-    if (si->si_code == 128) {
-        printf("[PASS] Got #GP as expected in lass\n");
-        exit(0);
-    }
-    else
-    {
-        printf("[FAIL] Got unexpected si_code:%d in lass\n",
-            si->si_code);
-        exit(1);
-    }
-    exit(1);
+	if (si->si_code == 128) {
+		printf("[PASS] Got #GP as expected in lass\n");
+		exit(0);
+	} else {
+		printf("[FAIL] Got unexpected si_code:%d in lass\n",
+			si->si_code);
+		exit(1);
+	}
+	exit(1);
 }
 
 int main(void)
@@ -40,7 +43,7 @@ int main(void)
 	}
 
 	hack_a = *(const int *)0xffffffffff600000;
-    printf("0xffffffffff600000 content:0x%x\n", hack_a);
+	printf("0xffffffffff600000 content:0x%x\n", hack_a);
 	printf("[FAIL]: should not read kernel space in lass\n");
-    return 1;
+	return 1;
 }
